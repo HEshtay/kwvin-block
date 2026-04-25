6867c6850f641cd471de8688a190a763eeab9965
Hoseen-PD <eshtay@doehle.de>
2026-04-25 11:31:32 +0200

    feat: register programType and programmePageType schemas

---
+++ next-sanity-site/src/sanity/schemaTypes/index.ts
@@ -0,0 +1,21 @@
+import { contactPageType } from "./contactPageType";
+import { homePageType } from "./homePageType";
+import { methodPageType } from "./methodPageType";
+import { methodPillarType } from "./methodPillarType";
+import { programmePageType } from "./programmePageType";
+import { programType } from "./programType";
+import { serviceType } from "./serviceType";
+import { siteChromeType } from "./siteChromeType";
+import { testimonialType } from "./testimonialType";
+
+export const schemaTypes = [
+  siteChromeType,
+  homePageType,
+  methodPageType,
+  programmePageType,
+  contactPageType,
+  serviceType,
+  testimonialType,
+  methodPillarType,
+  programType,
+];
